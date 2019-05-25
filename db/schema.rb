d3221fee76827fba498635986a66770d8ed03289
Candice Marx <candice.marx@hotmail.com>
2019-05-25 08:32:35 -0600

    created CRUD in blog

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_05_23_214700) do
+ActiveRecord::Schema.define(version: 2019_05_23_230015) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
